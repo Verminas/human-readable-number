3454022df7dc673429e4248dc32e6a3b07f561c3
verminas <veremeeva96@gmail.com>
2023-08-09 13:11:04 +0300

    feat: add solution for 0-9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+  let arrayOfNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let arrayOfStrings = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let length = String(number).length;
+  let stringFinished = '';
+  if (length == 1){ // проверка числа намбер на соответствие промежутку 0-9
+    for (let i = 0; i < arrayOfNumbers.length; i++){
+        if (arrayOfNumbers[i] == number){
+            stringFinished = arrayOfStrings[i];
+            break;
+        } else return 'number no from 0 to 9'; // пока не прописываю код для цифр больше 9
+    }
+  } else return 'length no 1'; // пока не прописываю код для десятков и сотен
+  return stringFinished;
 }
